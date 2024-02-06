5f7919e00082f674f1de0d9f97b4b58b9edf8585
ifloria <139184487+ifloria@users.noreply.github.com>
2024-02-07 00:00:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+
+lettersnumbers = {
+    "a": ".-", 
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "ı": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+    " ": " / ",
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----."
+}
+
+writing = input("writing: ")
+process = ""
+
+for fl in writing.lower():
+    if fl in lettersnumbers:
+        process += lettersnumbers[fl] + " "
+    else:
+        process += f"This Tool Does Not Support Some Letters! Unsupported Letter:({fl}) "
+
+os.system('cls')
+print('Results: ' + process)
